77292f48167ae938ef0844fea4f1fcc0f959552a
Nick Gentile <nickjgentile@gmail.com>
2017-10-27 19:07:20 -0700

    adding https fixes

--- src/js/components/info/infoActions.js
+++ src/js/components/info/infoActions.js
@@ -4,6 +4,6 @@ export default (city) => ({
     type: 'GET_WEATHER',
     meta: { city },
     payload: axios.get(
-        'http://api.openweathermap.org/data/2.5/weather?APPID=c803006286596219e500ae5a7afe68a7&units=imperial&q=' + city,
+        'https://api.openweathermap.org/data/2.5/weather?APPID=c803006286596219e500ae5a7afe68a7&units=imperial&q=' + city,
     ).then(axiosResponse => axiosResponse.data)
   });
